4ba13f616e2831ed1fb4f74c35570dc4a10ba9aa
Guilherme Almeida <guilherme_rna@hotmail.com>
2023-02-22 16:31:22 -0300

    melhoria no endpoint

--- src/main/java/com/example/demo/TesteController.java
+++ src/main/java/com/example/demo/TesteController.java
@@ -1,19 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesteController {
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping
 	public String hello() {
-		return "Hello";
+		
+		return "Hello, Port " + getPort();
 	}
 	
 	@GetMapping("{nome}")
 	public String helloNome(@PathVariable String nome) {
-		return "Hello " + nome;
+		return "Hello " + nome + " Port " + getPort();
+	}
+	
+	private String getPort() {
+		return env.getProperty("local.server.port").toString();
 	}
 }
